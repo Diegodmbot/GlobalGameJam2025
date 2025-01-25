3a5200a6c98d2e4239652d6235b7279eada63b47
Diego Díaz Morón <alu0101337975@ull.edu.es>
2025-01-25 18:54:02 +0000

    Papel añadido

--- node_2d.gd
+++ node_2d.gd
@@ -1,9 +1,13 @@
 extends CanvasLayer
 
-@onready var contador: Label = $Main/VBoxContainer/Contador
+@onready var contador: Label = $Main/Contador
 @onready var mejoras: Control = $Mejoras
 
 @onready var ADDER : int = 1
 
 func _on_button_pressed() -> void:
+	contador.text = str(int(contador.text) + ADDER * mejoras.multiplicador)
+
+
+func _on_texture_button_pressed():
 	contador.text = str(int(contador.text) + ADDER * mejoras.multiplicador)
